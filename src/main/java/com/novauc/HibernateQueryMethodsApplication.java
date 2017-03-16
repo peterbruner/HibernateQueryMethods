b1ff49ce2160ee1b21aaf6b2484cd00255b0a2cc
michael fahrner <michael.fahrner@theironyard.com>
2017-03-16 09:29:58 -0400

    Initial HibernateQueryMethods

---
+++ src/main/java/com/novauc/HibernateQueryMethodsApplication.java
@@ -0,0 +1,12 @@
+package com.novauc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateQueryMethodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateQueryMethodsApplication.class, args);
+	}
+}
